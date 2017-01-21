b8ad448031d91e82d64cae35f969a7c8d3f7bf05
Kvalvaag <PetterMoe.Kvalvaag@Emerson.com>
2017-01-21 23:57:05 +0100

    Adds more stuff to packages.el

--- packages.el
+++ packages.el
@@ -5,3 +5,22 @@
 (require 'powerline)
 (powerline-default-theme)
 
+;; EVIL mode
+(require 'evil)
+(evil-mode 1)
+(setq evil-default-cursor (quote (t "#750000"))
+    evil-visual-state-cursor '("#880000" box)
+    evil-normal-state-cursor '("#750000" box)
+    evil-insert-state-cursor '("#e2e222" bar)
+    )
+
+(setq helm-ff-transformer-show-only-basename nil
+      helm-ls-git-show-abs-or-relative 'relative)
+; Add cmake listfile names to the mode list.
+(setq auto-mode-alist
+	  (append
+	   '(("CMakeLists\\.txt\\'" . cmake-mode))
+	   '(("\\.cmake\\'" . cmake-mode))
+	   auto-mode-alist))
+
+(autoload 'cmake-mode "~/.emacs.d/cmake/cmake-mode.el" t)
