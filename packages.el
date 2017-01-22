6760e34cd08f0d1e51392d294b43e443050206d5
Petter Moe Kvalvaag <petter.kvalvaag@gmail.com>
2017-01-22 19:15:43 +0100

    Add evil surround and evil matchit

--- packages.el
+++ packages.el
@@ -7,12 +7,17 @@
 
 ;; EVIL mode
 (require 'evil)
+(require 'evil-surround)
 (evil-mode 1)
+(global-evil-surround-mode 1)
 (setq evil-default-cursor (quote (t "#750000"))
     evil-visual-state-cursor '("#880000" box)
     evil-normal-state-cursor '("#750000" box)
     evil-insert-state-cursor '("#e2e222" bar)
     )
+
+(require 'evil-matchit)
+(global-evil-matchit-mode 1)
 
 (setq helm-ff-transformer-show-only-basename nil
       helm-ls-git-show-abs-or-relative 'relative)
